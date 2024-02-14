56061a680db60796a718ecc232dc4f30e2bc7c67
LuckyShuii <lucasnimes30000@gmail.com>
2024-02-14 12:12:09 +0100

    qeuest 15 CRUD season & episode

---
+++ src/Form/EpisodeType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Episode;
+use App\Entity\Season;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EpisodeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('number')
+            ->add('synopsis')
+            ->add('season', EntityType::class, [
+                'class' => Season::class,
+                'choice_label' => 'number',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Episode::class,
+        ]);
+    }
+}
